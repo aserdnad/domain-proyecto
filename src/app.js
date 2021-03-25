3aa07520d16c94ebed513e23ff78745055b3ff61
aserdnad <andreasgamardo@gmail.com>
2021-03-26 00:46:09 +0100

    proyecto terminado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,32 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+
+// let uListaDominios = document.querySelector(".lista-dominios");
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let final = [".us", ".com"];
+let dominios = [];
+let unoXuno = "";
+
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let k = 0; k < noun.length; k++) {
+      for (let l = 0; l < final.length; l++) {
+        dominios.push(`${pronoun[i]}${adj[j]}${noun[k]}${final[l]}`);
+      }
+    }
+  }
+}
+
+let listaConLi = [];
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  dominios.forEach(dominio => console.log(dominio));
+  listaConLi = dominios.map(dominio => `<li class="p-3">${dominio}</li>`);
+  unoXuno += listaConLi.join(" ");
+  document.querySelector("#lista-dominios").innerHTML = unoXuno;
 };
